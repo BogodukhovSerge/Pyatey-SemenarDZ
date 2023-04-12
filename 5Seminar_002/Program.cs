a840c949a16e3979177c6a4926b79c3b2067f657
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-12 16:54:37 +0300

    DZ_2 got

--- 5Seminar_002/Program.cs
+++ 5Seminar_002/Program.cs
@@ -4,6 +4,13 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+int Bass(string sms)
+{
+    System.Console.WriteLine(sms);
+    string readImput = System.Console.ReadLine();
+    int result = int.Parse(readImput);
+    return result; 
+}
 
 int[] GeneratorArray(int Lenght, int minValui, int maxValua)
 {
@@ -19,7 +26,7 @@ int[] GeneratorArray(int Lenght, int minValui, int maxValua)
 
 int MetodNum(int sum)
 {
-    int[] numA = new int[i];
+    int[] numA = new int["0"];
     int lenght = 0;
     for ( int i = 0; i <= lenght; i++)
     {
@@ -28,5 +35,9 @@ int MetodNum(int sum)
     return sum;
 }
 
+int Lenght = Bass("Dlena maseva: ");
+int min = Bass("Nachalnoe znach, dlya leapazona sluchayn chisel: ");
+int max = Bass("Konechnoe znach, dlya leapazona sluchayn chisel: ");
 int [] array = GeneratorArray(Lenght, min, max);
-MetodNum(sum);
+System.Console.WriteLine($"otrecat chisel {array} = {MetodNum(sum)}");
+
