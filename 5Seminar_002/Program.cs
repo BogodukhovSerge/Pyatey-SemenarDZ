eab55b63b6735e0c7f6f49530b3a0c6e4ce53632
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-12 16:15:49 +0300

    DZ_2 Chet

--- 5Seminar_002/Program.cs
+++ 5Seminar_002/Program.cs
@@ -4,3 +4,16 @@
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
 
+
+int[] GeneratorArray(int Lenght, int minValui, int maxValua)
+{
+    int[] array = new int[Lenght];
+    Random random = new Random();
+    for (int i = 0; i < Lenght; i++)
+    {
+        array[i] = random.Next(minValui, maxValua + 1);
+    }
+    return array;
+
+}  
+
