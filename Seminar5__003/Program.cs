8ec27ccedc0f5365ba3e0587aad2ec065797c85b
Serge Bogoduhov <zz9250347500@gmail.com>
2023-04-13 14:11:25 +0300

    DZ_3 Chet

--- Seminar5__003/Program.cs
+++ Seminar5__003/Program.cs
@@ -3,3 +3,25 @@
 // [3 7 22 2 78] -> 76
 
 
+float[] GeneratorArray(float Lenght, float minValui, float maxValua)
+{
+    float[] array = new float[Lenght];
+    Random random = new Random();
+    for (int i = 0; i < Lenght; i++)
+    {
+        array[i] = random.Next(minValui, maxValua + 1);
+    }
+    return array;
+
+}  
+
+int MetodSum(int Sum)
+{
+    int min = 0;
+    int max = 0;
+    for (int i = 0; i > Lenght; i++)
+    {
+        if (min > i) min = i;
+        if (max < i) max = i;
+    }
+}
